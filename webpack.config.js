11839e367271bf738db6d964ef010ee7fb2747bb
aberrantian <aberrantian@gmail.com>
2023-12-10 00:41:43 +0000

    setup dev server

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,11 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
