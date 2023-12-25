7144c9147365b166a415006fb0c397d579768594
aberrantian <aberrantian@gmail.com>
2023-12-25 18:56:09 +0000

    setup project base

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
-// DON'T FORGET TO READ WEBPACK CODE SPLITTING GUIDE
-import ICON from './assets/icon.png';
+import './style.css';
 
-const HTML = document.getElementById('html');
-HTML.style.margin = '0px';
-HTML.style.height = '100%';
-HTML.style.width = '100%';
-const BODY = document.getElementById('body');
-BODY.style.margin = '0px';
-BODY.style.height = '100%';
-BODY.style.width = '100%';
-const CONTENT = document.getElementById('content');
-CONTENT.style.margin = '0px';
-CONTENT.style.height = '100%';
-CONTENT.style.width = '100%';
+const TAB_BAR = document.createElement('div');
+TAB_BAR.id = 'tab-bar';
 
-const LOGO_SIZE = '500px';
-const LOGO = new Image();
-LOGO.src = ICON;
+const CONTENT = document.createElement('div');
+CONTENT.id = 'content';
 
-const HEADLINE = document.createElement('h1');
-HEADLINE.innerText = 'SUSTENANCE from BIG CORP!';
 
-const DESCRIPTION = document.createElement('p');
-DESCRIPTION.innerText = 'Fully compliant with the Human Extinction Prevention Act of 2077 and approved by BIG ROBOT and its subsidiary BIG FOOD';
 
-CONTENT.append(LOGO, HEADLINE, DESCRIPTION);
+const HOME = document.createElement('button');
+HOME.innerText = 'Home';
+HOME.classList.add('tab-btn');
+const MENU = document.createElement('button');
+MENU.innerText = 'Menu';
+MENU.classList.add('tab-btn');
+const CONTACT = document.createElement('button');
+CONTACT.innerText = 'Contact';
+CONTACT.classList.add('tab-btn');
+
+TAB_BAR.append(HOME, MENU, CONTACT);
+document.body.append(TAB_BAR, CONTENT);
