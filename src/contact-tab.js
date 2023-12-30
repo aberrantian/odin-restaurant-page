9a75f5d134f6c82d1fc81c707cd8d4245ede989d
aberrantian <aberrantian@gmail.com>
2023-12-30 02:08:07 +0000

    populate contact page

--- src/contact-tab.js
+++ src/contact-tab.js
@@ -1,5 +1,33 @@
+import BIG_CORP_IMG from './assets/big_corp.png';
+import BIG_ROBOT_IMG from './assets/big_robot.png';
+import BIG_FOOD_IMG from './assets/big_food.png';
+
 export function createContact() {
-  const P = document.createElement('p');
-  P.innerText = 'Hello from contact';
-  return [P];
+  function createContactItem(image, name) {
+    const CONTAINER = document.createElement('div');
+    const IMAGE = new Image();
+    IMAGE.src = image;
+
+    const NAME = document.createElement('p');
+    NAME.innerText = name;
+
+    const NUMBER = document.createElement('p');
+    NUMBER.innerText = 'Phone Number: unavailable';
+
+    const EMAIL = document.createElement('p');
+    EMAIL.innerText = 'Email: unavailable';
+
+    const DESCRIPTION = document.createElement('p');
+    DESCRIPTION.innerText = 'Description: unavailable';
+
+    CONTAINER.append(IMAGE, NAME, NUMBER, EMAIL, DESCRIPTION);
+
+    return CONTAINER;
+  }
+
+  const BIG_CORP_ITEM = createContactItem(BIG_CORP_IMG, 'BIG CORP');
+  const BIG_ROBOT_ITEM = createContactItem(BIG_ROBOT_IMG, 'BIG ROBOT');
+  const BIG_FOOD_ITEM = createContactItem(BIG_FOOD_IMG, 'BIG FOOD');
+
+  return [BIG_CORP_ITEM, BIG_ROBOT_ITEM, BIG_FOOD_ITEM];
 }
