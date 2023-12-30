f658cfa8889578d8b78bf58d9cb3e7d35173ed74
aberrantian <aberrantian@gmail.com>
2023-12-30 02:11:17 +0000

    change default tab to home

--- src/index.js
+++ src/index.js
@@ -49,5 +49,5 @@ function setContent(content_array) {
 TAB_BAR.append(HOME, MENU, CONTACT);
 document.body.append(TAB_BAR, CONTENT);
 
-const DEFAULT_TAB = createContact();
+const DEFAULT_TAB = createHome();
 setContent(DEFAULT_TAB);
